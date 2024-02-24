5efdf48c3f59e81aec1ba2bb9a09509156bd77c4
Konova Ekaterina <konova.ek@mail.ru>
2024-02-24 11:48:52 +0000

    read the start and end times of conversations and in the conversations array we increase the values ​​by 1 during this period

--- script.sh
+++ script.sh
@@ -3,3 +3,10 @@
 read n
 
 conversations=()
+
+for ((i=0; i<n; i++)); do
+    read start end
+    for ((j=$start; j<=$end; j++)); do
+        conversations[$j]=$((conversations[$j] + 1))
+    done
+done
