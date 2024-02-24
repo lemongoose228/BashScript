f6344be2de9106915af090061a65ff4630051397
Konova Ekaterina <konova.ek@mail.ru>
2024-02-24 12:14:53 +0000

    find the maximum number of conversations and the earliest second with that number

--- script.sh
+++ script.sh
@@ -5,8 +5,18 @@ read n
 conversations=()
 
 for ((i=0; i<n; i++)); do
-    read start end
-    for ((j=$start; j<=$end; j++)); do
-        conversations[$j]=$((conversations[$j] + 1))
-    done
+	read start end
+	for ((j=$start; j<=$end; j++)); do
+		conversations[$j]=$((conversations[$j] + 1))
+	done
+done
+
+max_conversations=0
+max_time=0
+
+for ((i=0; i<=10000; i++)); do
+	if ((conversations[$i] > max_conversations)); then
+		max_conversations=${conversations[$i]}
+		max_time=$i
+	fi
 done
